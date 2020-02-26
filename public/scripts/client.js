73c394133414aba45e4e928d1012cabbc52a4805
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-02-26 04:05:24 +0000

    modified index, client, and tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,10 +8,12 @@
 
 
 const createTweetElement = (tweetData) => {
-  let $tweet = $(`
+  let date = new Date(tweetData['created_at'])
+
+  let $tweet = `
   <article class='old-tweet'>
           <header>
-            <img class='profile-pic' src="${tweetData['user']['avatars']}" alt=""> ge</img>
+            <img class='profile-pic' src="${tweetData['user']['avatars']}" alt=""> </img>
             <p2 class='name' >${tweetData['user']['name']}</p2>
             <p2 class='name-id'>${tweetData['user']['handle']}</p2>
           </header>
@@ -19,30 +21,56 @@ const createTweetElement = (tweetData) => {
             ${tweetData['content']['text']}
           </p>
           <footer >
-            <h6 class='tweet-date'>${tweetData['user']['created_at']}</h6>
+            <h6 class='tweet-date'>${date.toString()}</h6>
             <img name='tweet-icons' src="" alt="">test </img>
           </footer>
 
-        </article> `).addClass('tweet');
-
+        </article> `;
   return $tweet
 }
 
-
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
+const renderTweets = (tweets) => {
+  for(let tweet of tweets) {
+    $('#tweets-container').append(createTweetElement(tweet))
+  }
 }
 
-const $tweet = createTweetElement(tweetData);
+const loadTweets = () => {
+  $(document).ready( function(){
+    $.ajax({
+      type: 'GET',
+      url: '/tweets/'
+    }) .then( (data) => {
+      console.log(data);
+    })
+  })
+}
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+loadTweets()
+
+
+$(document).ready( function(){
+  const $button = $('.submitTweet');
+  const $text = $('.inputField')
+  console.log('worked!')
+  $button.on('click', function () {
+    console.log($text.serialize())
+    $.ajax({
+      type: 'POST',
+      url: '/tweets/',
+      data: $text.serialize()
+    })
+  })
+
+})
+
+
+
+
+
+$(document).ready(function() {
+  renderTweets(data);
+})
+
+
+ 
